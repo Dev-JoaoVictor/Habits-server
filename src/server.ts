359b96afc1376d2157463f8650da2db0551146ce
João <joaovictor.ramos34@gmail.com>
2023-03-01 12:06:42 -0300

    Criação da rota de novo hábito

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
-import { prisma } from "./lib/prisma";
-import { appRoutes } from "./lib/routes";
+
+import { appRoutes } from "./routes";
 
 const app = Fastify();
 
