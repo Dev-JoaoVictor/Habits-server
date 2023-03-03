9555463d02ffd2b4496c116f81f67fe0ee9fcc92
João <joaovictor.ramos34@gmail.com>
2023-03-03 12:20:13 -0300

    Criando as configurações para fazer o deploy

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,8 @@ app.register(appRoutes)
 
 app
   .listen({
-    port: 3333,
     host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
   })
   .then(() => {
     console.log("Server is running on PORT 3333");
